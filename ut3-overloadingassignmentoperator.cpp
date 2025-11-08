f3058083050e9a7c71a5ca9a6bbe7823a6db62a2
11249a157-jpg <11249a157@kanchiuniv.ac.in>
2025-11-08 12:18:20 +0530

    ut3-overloadingassignmentoperator.cpp

---
+++ ut3-overloadingassignmentoperator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring> // for strcpy
+using namespace std;
+
+class Book {
+    char title[50];
+    float price;
+
+public:
+    void getData() {
+        cout << "Enter book title: ";
+        cin >> title;
+        cout << "Enter price: ";
+        cin >> price;
+    }
+
+    // Overload assignment operator
+    void operator=(Book &b) {
+        strcpy(title, b.title);
+        price = b.price;
+    }
+
+    void display() {
+        cout << "Title: " << title << ", Price: " << price << endl;
+    }
+};
+
+int main() {
+    Book b1, b2;
+
+    b1.getData();
+    b2 = b1;  // calls overloaded assignment operator
+
+    cout << "\nCopied Book Details:\n";
+    b2.display();
+
+    return 0;
+}
